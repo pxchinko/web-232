b082dea286f09816e6121e2be559400c57d27aab
Devan Wong <devan.wong@yahoo.com>
2020-05-29 17:33:40 -0700

    changed variables

--- Week 1/wong-assignment-1.4.js
+++ Week 1/wong-assignment-1.4.js
@@ -1,9 +1,16 @@
-var firstName = 'Devan';
-var lastName = 'Wong';
-var age = 29;
-var street = '1234 California Street';
-var city = 'Oakland';
-var zipCode = 94501;
+var firstName;
+var lastName;
+var age;
+var street;
+var city;
+var zipCode;
+
+firstName = 'Devan';
+lastName = 'Wong';
+age = 29;
+street = '1234 California Street';
+city = 'Oakland';
+zipCode = 94501;
 
 console.log('Full name: '+ firstName + ' ' + lastName + ' ' + age);
 console.log('Random Address: ' + street + ', ' + city + ', ' + zipCode);
