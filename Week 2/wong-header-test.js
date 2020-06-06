ecc5cea86951aa1011ef914755440126490e70d9
Devan Wong <devan.wong@yahoo.com>
2020-06-05 20:49:24 -0700

    submit week 2 + header

---
+++ Week 2/wong-header-test.js
@@ -0,0 +1,12 @@
+/*
+============================================
+; Title:  header-test.js
+; Author: Professor Krasso
+; Date:    5 June 2020
+; Description: Displays a formatted header
+;===========================================
+*/
+
+const header = require('./wong-header.js');
+
+console.log(header.display('Devan', 'Wong', 'Assignment 2.2'));
