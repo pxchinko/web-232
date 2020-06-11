6a26a3a77511603871473e561be2119f2960696f
Sarah Massie <smassie@my365.bellevue.edu>
2020-06-10 21:48:08 -0700

    Update wong-assignment-3.1.js

--- Week 3/wong-assignment-3.1.js
+++ Week 3/wong-assignment-3.1.js
@@ -5,15 +5,16 @@ console.log(header.display('Devan', 'Wong', 'Assignment 3.1'))
 ; Title:  control statements.js
 ; Author:  Devan Wong
 ; Date:   6 June 2020
-; Modified By:
+; Modified By: Sarah Massie
 ; Description: 2 Errors please fix.
 ;===========================================
 */
 
-for (const i = 0, i <=10; i++) {
-  if (i == 5 ) {
-    console.log ('this hit five');
-  } else {
-    console.log('i = ' + i );
-  }
+for (var i = 0; i <= 10; i++) { // remove const because i is already declared
+  // i cannot be a const because it is a variable equal to a value, not a reference to a value
+if (i == 5) {
+  console.log ('this hit five');
+} else {
+  console.log('i = ' + i );
+}
 }
