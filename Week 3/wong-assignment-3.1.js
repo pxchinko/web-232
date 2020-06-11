753296e35bf258ffa5666975ceeb8c70b551ae08
Devan Wong <devan.wong@yahoo.com>
2020-06-10 18:12:05 -0700

    submit week 3 assignment 3.1

---
+++ Week 3/wong-assignment-3.1.js
@@ -0,0 +1,19 @@
+const header = require('../wong-header.js');
+console.log(header.display('Devan', 'Wong', 'Assignment 3.1'))
+/*
+============================================
+; Title:  control statements.js
+; Author:  Devan Wong
+; Date:   6 June 2020
+; Modified By:
+; Description: 2 Errors please fix.
+;===========================================
+*/
+
+for (const i = 0, i <=10; i++) {
+  if (i == 5 ) {
+    console.log ('this hit five');
+  } else {
+    console.log('i = ' + i );
+  }
+}
