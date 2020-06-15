df9a226a7f984ba86402126d6baaf04cb303cc14
Devan Wong <devan.wong@yahoo.com>
2020-06-14 22:32:44 -0700

    submit week 3 assignment 3.4

--- Week 3/wong-assignment-3.4.js
+++ Week 3/wong-assignment-3.4.js
@@ -10,33 +10,39 @@ console.log(header.display('Devan', 'Wong', 'Assignment 3.4'))
 ;===========================================
 */
 
-
-// Next, create an if...else statement and use the match function to test if the variable in step one matches the random number created in step 3.  For true, call the logMatch function using the same two values.  For false, call the logMismatch function using the same two values.  If it is not obvious by now, you are basically wrapping a for loop around the work you did in exercise 3.2.  And, instead of using multiple if...else statements and test variables you will be using a for loop to generate the test variables and one if..else statement to check the generated values.
-
 //create a variable between 1-10
 var arg1 = 6;
 
 for(var i = 0; i < 10; i++) {
-//random number function
-  var arg2 = Math.floor((Math.random() * 10) + 1);
-  //function to check to see if the random num matches the given number
-  function match(arg1,arg2) {
-    if (arg1 === arg2) {
-      return logMatch(arg1, arg2)
-    } else {
-        return logMismatch(arg1, arg2)
-      }
+  //calling random function
+  var arg2 = randomNumber();
+  //conditional to check to see if the random num matches the given number
+  if (match(arg1, arg2)) {
+    logMatch(arg1, arg2)
+  } else {
+    logMismatch(arg1, arg2)
+  }
+};
+
+//creating a function to create a random number
+function randomNumber() {
+  return Math.floor((Math.random() * 10) + 1)
+}
+
+//function to check to see if the random num matches the given number
+function match(arg1,arg2) {
+  if (arg1 === arg2) {
+    return true
+  } else {
+      return false;
+  }
+};
+
+  //boolean to have two strings that do not match
+  function logMismatch(arg1, arg2) {
+    console.log(arg1 + " and " + arg2 + " do not match!");
   };
-};
-
-//boolean to have two strings that do not match
-function logMismatch(arg1, arg2) {
-  console.log(arg1 + " and " + arg2 + " do not match!");
-};
-//boolean to have two strings that do match
-function logMatch(arg1, arg2) {
-  console.log(arg1 + " and " + arg2 + " do match!");
-};
-
-console.log(match(arg1,arg2));
-console.log(arg1,arg2);
+  //boolean to have two strings that do match
+  function logMatch(arg1, arg2) {
+    console.log(arg1 + " and " + arg2 + " do match!");
+  };
