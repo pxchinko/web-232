265fb77038b926b7a252a6e711c15685d850562a
Devan Wong <devan.wong@yahoo.com>
2020-06-18 15:17:32 -0700

    submit week 4 assignment 4.1

---
+++ Week 4/wong-assignment-4.1.js
@@ -0,0 +1,24 @@
+const header = require('../wong-header.js');
+console.log(header.display('Devan', 'Wong', 'Assignment 4.1'))
+/*
+============================================
+; Title:  Arrays js
+; Author:  Devan Wong
+; Date:   18 June 2020
+; Modified By:
+; Description: Using arrays 2 errors. Ready set go!
+;===========================================
+*/
+
+
+var a = [];
+
+a.push(1,2,3);    //output [1,2,3]
+a.pops();          //output [1,2]
+a.reverse();      //output [2,1]
+a.push(8,5,2);    //output [2,1,8,5,2]
+a.sort;         //output [1,2,2,5,8]
+
+
+//output [1,2,2,5,8]
+console.log(a);
