0da45221627037f288e35dcf751604b3a1c3c4b9
Sarah Massie <smassie@my365.bellevue.edu>
2020-06-21 00:58:31 -0700

    Update wong-assignment-4.1.js

--- Week 4/wong-assignment-4.1.js
+++ Week 4/wong-assignment-4.1.js
@@ -14,10 +14,10 @@ console.log(header.display('Devan', 'Wong', 'Assignment 4.1'))
 var a = [];
 
 a.push(1,2,3);    //output [1,2,3]
-a.pops();          //output [1,2]
+a.pop();          //output [1,2], misspelled as pops so function would not work
 a.reverse();      //output [2,1]
 a.push(8,5,2);    //output [2,1,8,5,2]
-a.sort;         //output [1,2,2,5,8]
+a.sort();         //output [1,2,2,5,8],  sort had no parentheses so it did not have any parameters to run as a function
 
 
 //output [1,2,2,5,8]
